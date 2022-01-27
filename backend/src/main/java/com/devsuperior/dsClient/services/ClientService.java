0cf6913a59ac13f6edb0fc373abcebc4e9c099a5
Renata Nitzsche <renata.nitzsche@hotmail.com>
2022-01-27 17:41:24 -0300

    auditing data

--- backend/src/main/java/com/devsuperior/dsClient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsClient/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsClient.dto.ClientDTO;
 import com.devsuperior.dsClient.entities.Client;
 import com.devsuperior.dsClient.repositories.ClientRepository;
+import com.devsuperior.dsClient.services.exceptions.DataBaseException;
 import com.devsuperior.dsClient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -67,5 +70,17 @@ public class ClientService {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
 		
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
+		}
 	}
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Bruna Souza', '05400312674', 5000.0, TIMESTAMP WITH TIME ZONE '2000-07-21T20:59:19Z', 2);  
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Arthur Nitzsche', '05305317670', 5700.0, TIMESTAMP WITH TIME ZONE '1977-06-21T20:59:19Z', 1);
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Bruno Mello', '05500312630', 4000.0, TIMESTAMP WITH TIME ZONE '1978-04-21T20:59:19Z', 2);  
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Lucia Santos', '88805319071', 3700.0, TIMESTAMP WITH TIME ZONE '1985-03-21T20:59:19Z', 3);    
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Bruna Souza', '05400312674', 5000.0, TIMESTAMP WITH TIME ZONE '2000-07-21T20:59:19Z', 2, NOW());  
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Arthur Nitzsche', '05305317670', 5700.0, TIMESTAMP WITH TIME ZONE '1977-06-21T20:59:19Z', 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Bruno Mello', '05500312630', 4000.0, TIMESTAMP WITH TIME ZONE '1978-04-21T20:59:19Z', 2, NOW());  
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Ana Lucia Santos', '88805319071', 3700.0, TIMESTAMP WITH TIME ZONE '1985-03-21T20:59:19Z', 3, NOW());    
